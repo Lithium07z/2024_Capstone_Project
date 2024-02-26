c9ab3d4a4214f3477c6d3d0e00740f8d21bb4459
Mellow1213 <dpcks7282@gmail.com>
2024-02-26 13:03:35 +0900

    Room Creation

---
+++ Assets/02.Script/Photon/PhotonManager.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    
+
+    void Awake()
+    {
+#if UNITY_STANDALONE_WIN
+        Screen.SetResolution(1366, 768, false);
+#endif
+
+        Debug.Log("Connecting...");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Connected to Server Sucessfully");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        PhotonNetwork.JoinOrCreateRoom("Room", null, null);
+        
+        Debug.Log("You Entered the Room Name: " + "Room");
+
+        Invoke("InstantiatePlayer", 5f);
+        
+    }
+
+    void InstantiatePlayer()
+    {
+        PhotonNetwork.Instantiate("PlayerArmature", Vector3.zero, Quaternion.identity);
+    }
+}
+
+/*
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public Text StatusText;
+    public InputField NickNameInput;
+    public string gameVersion = "1.0";
+
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start()
+    {
+        PhotonNetwork.GameVersion = this.gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("PlayerArmature", Vector3.zero, Quaternion.identity);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        this.CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+    }
+}
+*/
