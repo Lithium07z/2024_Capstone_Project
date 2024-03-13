7ab17f89a55e6ba4705abbd26e1d5e7d795a994d
Mellow1213 <dpcks7282@gmail.com>
2024-03-13 15:00:23 +0900

    총기류 IK 구현 완료

--- Assets/02.Script/Attack/GunShooter.cs
+++ Assets/02.Script/Attack/GunShooter.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 using Photon.Pun;
 using StarterAssets;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class GunShooter : MonoBehaviour
 {
     public GameObject gun;
+    public Transform gunPivot;
+    private Transform leftHandIKPivot;
     
+    // Character's Component
     private Animator _anim;
     private StarterAssetsInputs _input;
-    private GunProperty _gunProperty;
-    
-    public Transform target; // 총구의 위치
-    public Transform gunHandle; // 총 잡는 위치
 
+    public Transform temp;
+    
+    // Current Weapon's Property Class Ref
+    private GunProperty _gunProperty;
+    private GameObject currentGun;
+
+    
     private bool isAiming = false; // 현재 에임 상태
     
     // Animator Parameters
@@ -26,24 +33,19 @@ public class GunShooter : MonoBehaviour
     {
         _input = GetComponent<StarterAssetsInputs>();
         _anim = GetComponent<Animator>();
+
+        currentGun = Instantiate(gun, gunPivot.transform.position, gunPivot.transform.rotation);
+        currentGun.transform.parent = gunPivot;
+        _gunProperty = currentGun.GetComponent<GunProperty>();
+        leftHandIKPivot = _gunProperty.leftIKPivot.transform;
     }
+    
 
     private void Update()
     {
-    
-        _gunProperty = gun.GetComponent<GunProperty>();
-        // 에임 입력 감지
-        if (_input.aim && !isAiming)
-        {
-            isAiming = true;
-            _anim.SetBool(_animIDAim, true);
-        }/*
-        else if (!_input.aim && isAiming)
-        {
-            isAiming = false;
-            _anim.SetBool(_animIDAim, false);
-        }*/
-
+        Aim();
+        Debug.Log(leftHandIKPivot.gameObject.name);
+        Debug.Log("Position From GunShooter : " + leftHandIKPivot.position);
         // 발사 로직 (추가 예정)
         /*
         if (_input.shoot)
@@ -53,11 +55,28 @@ public class GunShooter : MonoBehaviour
         */
     }
 
-    void Shoot()
+    void Aim()
+    {
+        // 에임 입력 감지
+        if (_input.aim && !isAiming)
+        {
+            isAiming = true;
+            _anim.SetBool(_animIDAim, true);
+        }
+        else if (!_input.aim && isAiming)
+        {
+            isAiming = false;
+            _anim.SetBool(_animIDAim, false);
+        }
+    }
+    
+    void Attack()
     {
         // 발사 메커니즘 구현
         // 예: 총알 발사, 사운드 재생, 애니메이션 트리거 등
     }
+
+    
 
     private void OnAnimatorIK(int layerIndex)
     {
@@ -65,13 +84,11 @@ public class GunShooter : MonoBehaviour
         {
             _anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
             _anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
-            _anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
-            _anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
 
-            _anim.SetIKPosition(AvatarIKGoal.LeftHand, _gunProperty.LeftHandTarget.position);
-            _anim.SetIKRotation(AvatarIKGoal.LeftHand, _gunProperty.LeftHandTarget.rotation);
-            _anim.SetIKPosition(AvatarIKGoal.RightHand, _gunProperty.RightHandTarget.position);
-            _anim.SetIKRotation(AvatarIKGoal.RightHand, _gunProperty.RightHandTarget.rotation);
+            _anim.SetIKPosition(AvatarIKGoal.LeftHand, leftHandIKPivot.position);
+            _anim.SetIKRotation(AvatarIKGoal.LeftHand, leftHandIKPivot.rotation);
+            //_anim.SetIKPosition(AvatarIKGoal.LeftHand, temp.position);
+            //_anim.SetIKRotation(AvatarIKGoal.LeftHand, temp.rotation);
         }
         else
         {
