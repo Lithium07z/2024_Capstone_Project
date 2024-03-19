1a4e6c0ee95d32f1a83f886c619c75cd4b8c1c9c
Mellow1213 <dpcks7282@gmail.com>
2024-03-19 16:11:33 +0900

    Folder Refactor & UIManager in Progress

---
+++ Assets/02.Script/UIManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIManager : MonoBehaviour
+{
+
+    private static UIManager instance;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null) 
+                instance = FindObjectOfType<UIManager>();
+            return instance;
+        }
+    }
+
+
+    [SerializeField] private Image crosshair;
+
+    
+    public void SetActiveCrosshair(bool state)
+    {
+        crosshair.enabled = state;
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
