b624f800178b8aaf3b1907d515ef6381107c765d
Sejin-Yun <yuntwo22@naver.com>
2024-03-26 21:26:45 +0900

    Update MainGUIManager.cs
    region과 주석 사용하여 코드 정리

--- Assets/02.Script/Managers/MainGUIManager.cs
+++ Assets/02.Script/Managers/MainGUIManager.cs
@@ -62,21 +62,40 @@ public class MainGUIManager : MonoBehaviour
         
     }
 
+    // PhotonNetwork 닉네임에 보내주는 Getter
+    public string GetUserName()
+    {
+        return nickNameInput.text;
+    }    
+    
+    #region 텍스트를 업데이트 하는 함수들 모음
+    // PhotonNetwork 상태를 디버깅하기 위해 필요한 텍스트를 업데이트 하는 함수
     public void UpdatePhotonStatusText(string _statusText)
     {
         statusText.text = _statusText;
     }
 
+    // 로비에 들어간 이후, 로비 인원 수 정보를 업데이트 하는 함수
     public void UpdateLobbyInfoText(int _lobbyPlayersCount, int _allPlayersCount)
     {
         lobbyInfoText.text =  _lobbyPlayersCount + " in Lobby / " + _allPlayersCount + " Connected";
     }
-
+    
+    // 방에 들어가면 타이머 텍스트를 업데이트 하는 함수
+    public void UpdateTimerText(int _minutes, int _seconds)
+    {
+        timerText.text = string.Format("{0:00}:{1:00}", _minutes, _seconds);
+    }
+    
+    // 방에 들어가면 방의 인원수를 업데이트 하는 함수
     public void RoomRenewal(int _roomPlayerCount, int _maxPlayerCount)
     {
         roomInfoText.text = _roomPlayerCount + " / " + _maxPlayerCount;
     }
-
+    
+    #endregion
+    
+    #region 버튼에 연결할 함수들 모음
     public void ConnectGame()
     {
         NetworkManager.Instance.Connect();
@@ -85,14 +104,14 @@ public class MainGUIManager : MonoBehaviour
         lobbyPanel.SetActive(true);
         roomPanel.SetActive(false);
     }
-
+    
     public void EnterRoom()
     {
         NetworkManager.Instance.JoinRandomRoom();
         lobbyPanel.SetActive(false);
         roomPanel.SetActive(true);
     }
-
+    
     public void LeaveRoom()
     {
         NetworkManager.Instance.LeaveRoom();
@@ -107,17 +126,8 @@ public class MainGUIManager : MonoBehaviour
         lobbyPanel.SetActive(false);
         roomPanel.SetActive(false);
     }
-
-    public void UpdateTimerText(int _minutes, int _seconds)
-    {
-        timerText.text = string.Format("{0:00}:{1:00}", _minutes, _seconds);
-    }
-
-    public string GetUserName()
-    {
-        return nickNameInput.text;
-    }
     
+    #endregion
     
     // #region 채팅
     // public void Send()
